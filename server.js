5cdfb26a90d7c50e136d2e4a52ed0a8d662bc330
Vu Nguyen <vu.nguyen101@gmail.com>
2020-08-15 08:56:44 +0300

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())
 const PORT = process.env.PORT || 3000
+
 
 const mockUserData = [
     { name: 'Mark' },
@@ -22,5 +25,26 @@ app.get('/users/:id', (req, res) => {
         message: 'got one user!',
         user: req.params.id,
     })
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    const mockUserName = 'billyTheKid'
+    const mockPassword = 'superSecret'
+
+    if (username === mockUserName && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'credentials match!',
+            token: 'encrypted token go here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'credentials do not match!',
+        })
+    }
 })
 app.listen(PORT, () => { console.log(`server is running on port ${PORT}`) })
